805e26e3b76f51bd9f2bfe60002ba4c9f86a34be
Vicente Figueroa <figueroa.vicente9@gmail.com>
2022-11-18 14:21:08 -0600

    Spring: corrected roll-dice exercise

--- src/main/java/com/example/springblog/DiceController.java
+++ src/main/java/com/example/springblog/DiceController.java
@@ -1,6 +1,7 @@
 package com.example.springblog;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -10,15 +11,21 @@ import java.util.Random;
 @Controller
 public class DiceController {
 
+    @GetMapping("/roll-dice")
+    public String showChoices() {
+        return "roll-dice";
+    }
+
     @GetMapping("/roll-dice/{n}")
-    @ResponseBody
-    public String diceGuess(@PathVariable int n) {
+    public String showResults(@PathVariable int n, Model model) {
         Random rand = new Random();
         int randomNum = rand.nextInt(6 - 1 + 1) + 1;
-        if (randomNum == n) {
-            return "Your number was " + n + ".\nThe number to guess was " + randomNum + ".\nYou guessed correctly.";
-        } else {
-            return "Your number was " + n + ".\nThe number to guess was " + randomNum + ".\nYou guessed incorrectly.";
-        }
+        boolean result = n == randomNum;
+        boolean notResult = n != randomNum;
+        model.addAttribute("result", result);
+        model.addAttribute("notResult", notResult);
+        model.addAttribute("randomNum", randomNum);
+
+        return "roll-dice";
     }
 }
--- src/main/java/com/example/springblog/PostController.java
+++ src/main/java/com/example/springblog/PostController.java
@@ -7,8 +7,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-
-import java.sql.Array;
 import java.util.ArrayList;
 import java.util.List;
 
