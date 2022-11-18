8476263bdf8c904cf643140de6addfffd5b3c443
Vicente Figueroa <figueroa.vicente9@gmail.com>
2022-11-18 14:50:22 -0600

    organized classes and templates

--- src/main/java/com/example/springblog/controllers/DiceController.java
+++ src/main/java/com/example/springblog/controllers/DiceController.java
@@ -1,10 +1,9 @@
-package com.example.springblog;
+package com.example.springblog.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.Random;
 
--- src/main/java/com/example/springblog/controllers/PostController.java
+++ src/main/java/com/example/springblog/controllers/PostController.java
@@ -1,4 +1,4 @@
-package com.example.springblog;
+package com.example.springblog.controllers;
 
 import com.example.springblog.models.Post;
 import org.springframework.stereotype.Controller;
@@ -16,8 +16,8 @@ public class PostController {
     @GetMapping("/posts")
     public String postsIndexPage(Model model) {
         List<Post> posts = new ArrayList<>();
-        Post post1 = new Post( "My First Post", "This is my first post using spring blog.");
-        Post post2 = new Post( "My Second Post", "This is my second post using spring blog.");
+        Post post1 = new Post(1, "My First Post", "This is my first post using spring blog.");
+        Post post2 = new Post(2, "My Second Post", "This is my second post using spring blog.");
         posts.add(post1);
         posts.add(post2);
         model.addAttribute("posts", posts);
@@ -26,9 +26,7 @@ public class PostController {
 
     @GetMapping("/posts/{id}")
     public String postsUserPage(@PathVariable long id, Model model) {
-        Post post = new Post(1, "My Individual Post", "This is my individual post.");
-        boolean idsMatch = id == post.getId();
-        model.addAttribute("idsMatch", idsMatch);
+        Post post = new Post(id, "My Individual Post", "This is my individual post.");
         model.addAttribute("post", post);
         return "posts/show";
     }
